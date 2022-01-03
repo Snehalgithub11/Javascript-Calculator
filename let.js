d9c2e4a8f4eed3d3552cf3bb9aad0122da80faf0
Snehalgithub11 <88919062+Snehalgithub11@users.noreply.github.com>
2022-01-03 13:55:51 +0530

    Add files via upload

---
+++ let.js
@@ -0,0 +1,23 @@
+let num1=10
+let num2=3
+let num3=0
+let num4=document.getElementById('result')
+function add(){
+    num3=num1+num2
+    num4.textContent= 'Result is'+num3
+
+}
+function sub(){
+    num3=num1-num2
+    num4.textContent='Result is'+num3
+}
+function mul(){
+    num3=num1*num2
+    num4.textContent='Result is'+num3
+}
+function div(){
+    num3=num1/num2
+    num4.textContent='Result is'+num3
+}
+
+
